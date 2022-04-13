25bdd9e9ed378034e4f8f393dbd359142e2630e6
VictorKmin <kminvictor@gmail.com>
2022-04-13 21:39:22 +0300

    Swagger

--- lesson10/src/router/apiRouter.ts
+++ lesson10/src/router/apiRouter.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
+import swaggerUI from 'swagger-ui-express';
 
 import { userRouter } from './userRouter';
 import { authRouter } from './authRouter';
 import { studentRouter } from './studentRouter';
+import docs from '../docs/swagger.json';
 
 const router = Router();
 
+router.use('/docs', swaggerUI.serve, swaggerUI.setup(docs));
 router.use('/users', userRouter);
 router.use('/auth', authRouter);
 router.use('/students', studentRouter);
